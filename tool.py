a060cdd0eade6ed45650ab2f8c90841f39bc274b
whitesnow888 <whitesnow3414@gmail.com>
2025-07-28 01:45:41 +0900

    1

--- tool.py
+++ tool.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
 import time
+import pandas as pd
 
 with sync_playwright() as p:
     # Launch Chromium browser
@@ -10,12 +11,28 @@ with sync_playwright() as p:
     page.goto("https://www.keishicho-gto.metro.tokyo.lg.jp/keishicho-u/reserve/offerList_detail?tempSeq=396&accessFrom=offerList")
     print("Navigated to the page")
 
+    # while True:
+    #     print("Reloading the page...")
+    #     # Reload the page
+    #     page.reload()
+    #     # Wait for the page to fully load
+    #     page.wait_for_load_state('load')
+    #     # Check if the #waitTime element exists
+    #     wait_time_element = page.query_selector('#waitTime')
+    #     if wait_time_element and wait_time_element.is_visible():
+    #         print("Found #waitTime element. Content loaded.")
+    #         break
+    #     else:
+    #         print("#WAITTIME not found or not visible, reloading again...")
+    #         time.sleep(2)  # optional small delay before next reload
+
+    # Wait for the "WAITTIME" element to disappear, indicating site is ready
+    page.wait_for_selector('#waitTime', state='detached', timeout=600000000)
+    print("Site is ready for interaction.")
+    
     # Click the login button
     page.click('.common-header-loginbtn')
     print("Clicked the login button")
-
-    # Wait for the page to fully load
-    page.wait_for_load_state('load')  # or 'networkidle' for a more robust wait
 
     # Fill in login info
     page.fill('input[id="userLoginForm.userId"]', "k-ikeda@technosmile.co.jp")
@@ -32,6 +49,7 @@ with sync_playwright() as p:
 
     for i in range(10):
         # Check if the radio button is disabled
+        page.wait_for_load_state('load', timeout=60000)  # Wait for 60 seconds
         radio_button = page.query_selector("input[value='1か月後＞']")
         if radio_button and radio_button.is_disabled():
             print("Radio button is disabled. Exiting loop.")
@@ -49,42 +67,40 @@ with sync_playwright() as p:
         # Click the radio button for "2週後＞"
         page.click("input[value='2週後＞']")
         print(f"Clicked radio button for '2週後＞' {i+1} times")
+            
     
-    # print("Please click on the target element to continue...")
-    # page.wait_for_selector('.time--table.time--th--date.bordernone.tdSelect.enable')
-    # page.eval_on_selector('.time--table.time--th--date.bordernone.tdSelect.enable', '''
-    #     element => new Promise(resolve => {
-    #         element.addEventListener('click', () => resolve());
-    #     })
-    # ''')
-    # print("Detected click on the target element. Continuing...")
-
+    for i in range(3, 17):  # Loop through columns
+        cell = page.query_selector(f"table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.tdSelect.enable")
+        # cell = page.query_selector('table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.disable')
+        if(cell):
+            print("Cell found with the specified class. Clicking the cell...")
+            cell.click()
+            break
+        else:
+            print(f"{i}Cell with the specified class not found.")
     
-    # page.wait_for_load_state('load')  # or 'networkidle' for a more robust wait
-    # page.click('input[class="c-btn_2 button-outline"]')
-    # page.wait_for_load_state('load')  # or 'networkidle' for a more robust wait
-    # page.click('input[class="c-btn_2 button-outline"]')
+    
+    time.sleep(1)
+    page.click('td#pc-0_6')
 
-    # print("Please click on the target element to continue...")
-    # page.wait_for_selector('.time--table.time--th--date.bordernone.tdSelect.enable')
-    # page.eval_on_selector('.time--table.time--th--date.bordernone.tdSelect.enable', '''
-    #     element => new Promise(resolve => {
-    #         element.addEventListener('click', () => resolve());
-    #     })
-    # ''')
-    # print("Detected click on the target element. Continuing...")
+    cell = page.query_selector('.c-btn_2.button-outline')
+    cell.click()
 
-    # print("Please click on the target element to continue...")
-    # page.wait_for_selector('.time--table.time--th.enable.bordernone.tdSelect')
-    # page.eval_on_selector('.time--table.time--th.enable.bordernone.tdSelect', '''
-    #     element => new Promise(resolve => {
-    #         element.addEventListener('click', () => resolve());
-    #     })
-    # ''')
-    # print("Detected click on the target element. Continuing...")
+    page.wait_for_load_state('load', timeout=60000)  # Wait for the page to load
+    cell = page.query_selector('.c-btn_2.button-outline')
+    cell.click()
+    print("current checking")
 
-    # Wait for 30 seconds
-    time.sleep(3000)
+    #read the main info from csv
+    # Read the Excel file
+    df = pd.read_excel('asdasd.xlsx', header=None)
+
+    # The second row contains the data values
+    data_values = df.iloc[1]
+
+    print("Data values:", data_values.tolist())
+    
+    time.sleep(300000)
     
     # Close the browser
     browser.close()
