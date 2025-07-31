31c284b81a2523739d970268e01b6a36c4b0061e
whitesnow888 <whitesnow3414@gmail.com>
2025-07-31 12:39:27 +0900

    1

--- tool.py
+++ tool.py
@@ -2,6 +2,8 @@ from playwright.sync_api import sync_playwright
 import time
 import pandas as pd
 
+
+
 with sync_playwright() as p:
     # Launch Chromium browser
     browser = p.chromium.launch(headless=False)  # headless=False to see the browser
@@ -56,22 +58,34 @@ with sync_playwright() as p:
         print(f"Clicked radio button for '2週後＞' {i+1} times")
             
     
-    for i in range(3, 17):  # Loop through columns
-        cell = page.query_selector(f"table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.tdSelect.enable")
-        # cell = page.query_selector('table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.disable')
-        if(cell):
-            print("Cell found with the specified class. Clicking the cell...")
-            cell.click()
-            break
-        else:
-            print(f"{i}Cell with the specified class not found.")
+    flg = 0
+    
+    while flg == 0:
+        time.sleep(3)
+        page.wait_for_selector('#waitTime', state='detached', timeout=600000000)
+        for i in range(3, 17):  # Loop through columns
+            cell = page.query_selector(f"table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.tdSelect.enable")
+            # cell = page.query_selector('table tbody tr:nth-child(9) td:nth-child({i}).time--table.time--th--date.bordernone.disable')
+            if(cell):
+                print("Cell found with the specified class. Clicking the cell...")
+                cell.click()
+                flg = 1
+                break
+            else:
+                print(f"{i}Cell with the specified class not found.")
+        if(flg == 0):
+            page.reload()  # Refresh the page to check for available time slots
     
     
     time.sleep(2)
 
+    if(flg == 0):
+        print("No available time slots found. Exiting.")
+        browser.close()
+        exit()
     page.click('td#pc-0_6')
-    page.click('td#pc-0_60')
     page.click('td#pc-0_66')
+    page.click('td#pc-0_60')
 
     # Click the Next button to proceed
     cell = page.query_selector('.c-btn_2.button-outline')
@@ -81,31 +95,26 @@ with sync_playwright() as p:
     cell = page.query_selector('.c-btn_2.button-outline')
     cell.click()
     
+    
     #read the main info from csv
     # Read the Excel file
     df = pd.read_excel('asd.xlsx', header=None)
 
     # The second row contains the data values
     data_values = df.iloc[:,1]
-
-    print("Data values:", data_values.tolist())
+    # print("Data values:", data_values.tolist())
 
     page.wait_for_load_state('load', timeout=60000)  # Wait for the page to load
     #input the data into the form
+
     page.fill('input[id="switch_8691"]', data_values[0])  # 国籍(1)
-    # page.fill('input[id="item_2_textData"]', str(data_values[1]))  # 郵便番号
-    # page.select_option('select[id="switch_8820_city"]', value=data_values[3])  # 住所（都道府県）
-    # time.sleep(1)  # Wait for the options to load
-    # page.select_option('select[id="switch_8820_town"]', value=data_values[4])  # 住所（町・大字）
-    # time.sleep(1)  # Wait for the options to load
-    # page.select_option('select[id="switch_8820_block"]', value=str(data_values[5]))  # 住所（丁目・字）
+    page.fill('input[id="item_2_textData"]', str(data_values[1]))  # 郵便番号
+    page.select_option('select[id="switch_8820_city"]', value=data_values[3])  # 住所（都道府県）
+    time.sleep(1)  # Wait for the options to load
+    page.select_option('select[id="switch_8820_town"]', value=data_values[4])  # 住所（町・大字）
+    time.sleep(1)  # Wait for the options to load
+    page.select_option('select[id="switch_8820_block"]', value=str(data_values[5]))  # 住所（丁目・字）
     
-
-
-
-
-
-
 
 
 
